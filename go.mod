4a35c984fe9cd6bfc329d6d7f355ad668f20a7e2
Tibor Kircsi <tkircsi@gmail.com>
2021-01-12 19:18:00 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tkircsi/sitemap
+
+go 1.15
+
+require github.com/tkircsi/link v0.0.0-20210111163203-21315a6d66d2
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/tkircsi/link"
+)
+
+func main() {
+	urlFlag := flag.String("url", "http://gophercises.com", "the url that you want to build a sitemap for")
+	flag.Parse()
+
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, err := link.Parse(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func filter(links []string, fn func(string) bool) []string {
+	var ret []string
+	for _, l := range links {
+		if fn(l) {
+			ret = append(ret, l)
+		}
+	}
+	return ret
+}
+
+func withPrefix(prefix string) func(string) bool {
+	return func(l string) bool {
+		return strings.HasPrefix(l, prefix)
+	}
+}
